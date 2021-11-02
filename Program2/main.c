5b29a5a2c6280a19c6f8b1828944541ba0e2a7e9
kcjennin <kcjennin@calpoly.edu>
2021-11-01 20:10:26 -0700

    Modifiying main

--- Program2/main.c
+++ Program2/main.c
@@ -3,13 +3,19 @@
 #include<stdio.h>
 
 int main(int argc, char *argv[]) {
-    char *s, *s2;
+    char *s, *s2, *s3;
 
     s = (char *)malloc(sizeof(char) * (2 << 15));
     s2 = (char *)malloc(sizeof(char) * (2 << 15));
 
     free(s);
     free(s2);
+
+    s3 = (char *)malloc(sizeof(char) * (2 << 17));
+
+    s3[(2 << 17) - 1] = 0;
+
+    free(s3);
 
     return 0;
 }
